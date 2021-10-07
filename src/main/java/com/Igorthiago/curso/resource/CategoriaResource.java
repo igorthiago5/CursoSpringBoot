e4caa0a8b98e32b8cf21d5c9350f78ce72b6284e
igor thiago <igorthiago255@hotmail.com>
2021-10-06 20:08:44 -0400

    Banco de dados criado(h2) e Classe Categoria recebeu o JPA

--- src/main/java/com/Igorthiago/curso/resource/CategoriaResource.java
+++ src/main/java/com/Igorthiago/curso/resource/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.Igorthiago.curso.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
